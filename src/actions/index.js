b1166cf2e1ec6b3e5c110a817f37792160546013
zitongliu <zitongliu7@gmail.com>
2017-06-30 14:44:34 +1000

    search bar ajax with axios - wip

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a880d0b73d392078f11b59747b04f2a5';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function = fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},AU`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
