8237e26234e0d2b05251460a330e956b30f86c01
zitongliu <zitongliu7@gmail.com>
2017-07-02 10:52:28 +1000

    set up ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'a880d0b73d392078f11b59747b04f2a5';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
