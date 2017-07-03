b9e3700d4f6413823cda1e4658452beb70e715db
zitongliu <zitongliu7@gmail.com>
2017-07-03 23:25:15 +1000

    weather reducer wip

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,6 +7,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},au`;
+  // axios returns a promise, we then pass the promise as the payload of this action
   const request = axios.get(url);
 
   return {
