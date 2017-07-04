05962c069d9bc9ee3a51b86813b15ef4b23c0563
zitongliu <zitongliu7@gmail.com>
2017-07-04 12:11:20 +1000

    created pressure and humidity charts

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ import {Sparklines, SparklinesLine} from 'react-sparklines';
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={props.data}>
+      <Sparklines width={120} height={80} data={props.data}>
         <SparklinesLine color={props.color} />
       </Sparklines>
     </div>
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,6 +6,8 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
     const temps = cityData.list.map( weather => weather.main.temp );
+    const pressures = cityData.list.map( weather => weather.main.pressure );
+    const humidities = cityData.list.map( weather => weather.main.humidity );
 
     return (
       <tr key={name}>
@@ -13,10 +15,16 @@ class WeatherList extends Component {
         <td>
           <Chart data={temps} color="orange" />
         </td>
+        <td>
+          <Chart data={pressures} color="green" />
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" />
+        </td>
       </tr>
     )
   }
-  
+
   render() {
     return (
       <table className="table table-hover">
