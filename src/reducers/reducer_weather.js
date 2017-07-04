dcabd0a23b06b88aa1ffd51b0345f5314b7607cd
zitongliu <zitongliu7@gmail.com>
2017-07-04 10:56:49 +1000

    weather reducer complete

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action ) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action ) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // concat does not replace the old state, it creates a new instance of state
+      // return state.concat([action.payload.data]);
+      return [action.payload.data, ...state ];
+  }
   return state;
 }
