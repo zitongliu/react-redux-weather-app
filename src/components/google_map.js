594700b40995890c4dbacae09029d11d843a027e
zitongliu <zitongliu7@gmail.com>
2017-07-10 16:54:52 +1000

    integrated google maps

---
+++ src/components/google_map.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+
+  render() {
+    // refs allows us to use this.refs.map elsewhere
+    return <div ref="map" />;
+  }
+}
+
+export default GoogleMap;
